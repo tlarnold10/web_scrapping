8551b1a33a7f73d548b0ac3e37ee7570bd568f30
Trevor <tlarnold10@hotmail.com>
2020-05-28 20:06:31 -0500

    continued work on this

--- console_app.py
+++ console_app.py
@@ -3,71 +3,88 @@ from bs4 import BeautifulSoup
 import requests
 import pdb
 
-# Lowes ===============================================
-my_results_list = []
+def main():
+    # Lowes ===============================================
+    my_results_list = []    
 
-query = "lowes WDT750SAHZ"
+    query = "lowes WDT750SAHZ"  
 
-for i in search(query,        # The query you want to run
-                tld = 'com',  # The top level domain
-                lang = 'en',  # The language
-                num = 1,     # Number of results per page
-                start = 0,    # First result to retrieve
-                stop = 1,  # Last result to retrieve
-                pause = 2.0,  # Lapse between HTTP requests
-               ):
-    my_results_list.append(i)
-    print(i)
+    for i in search(query,        # The query you want to run
+                    tld = 'com',  # The top level domain
+                    lang = 'en',  # The language
+                    num = 1,     # Number of results per page
+                    start = 0,    # First result to retrieve
+                    stop = 1,  # Last result to retrieve
+                    pause = 2.0,  # Lapse between HTTP requests
+                   ):
+        my_results_list.append(i)   
 
-pdb.set_trace()
-req = requests.get(my_results_list[0])
-soup = BeautifulSoup(req.content, 'html.parser')
-print(soup.prettify())
+    print(my_results_list[0])   
+    
 
-# Karls ==================================================
-my_results_list = []
+    # Karls ==================================================
+    my_results_list = []    
 
-query = "Karls WDT750SAHZ"
+    query = "Karls WDT750SAHZ"  
 
-for i in search(query,        # The query you want to run
-                tld = 'com',  # The top level domain
-                lang = 'en',  # The language
-                num = 1,     # Number of results per page
-                start = 0,    # First result to retrieve
-                stop = 1,  # Last result to retrieve
-                pause = 2.0,  # Lapse between HTTP requests
-               ):
-    my_results_list.append(i)
-    print(i)
+    for i in search(query,        # The query you want to run
+                    tld = 'com',  # The top level domain
+                    lang = 'en',  # The language
+                    num = 1,     # Number of results per page
+                    start = 0,    # First result to retrieve
+                    stop = 1,  # Last result to retrieve
+                    pause = 2.0,  # Lapse between HTTP requests
+                   ):
+        my_results_list.append(i)
+        print(i)
+        
+    get_price(my_results_list[0])   
 
-# Home Depot ================================================
-my_results_list = []
+    # Home Depot ================================================
+    my_results_list = []    
 
-query = "Home Depot WDT750SAHZ"
+    query = "Home Depot WDT750SAHZ" 
 
-for i in search(query,        # The query you want to run
-                tld = 'com',  # The top level domain
-                lang = 'en',  # The language
-                num = 1,     # Number of results per page
-                start = 0,    # First result to retrieve
-                stop = 1,  # Last result to retrieve
-                pause = 2.0,  # Lapse between HTTP requests
-               ):
-    my_results_list.append(i)
-    print(i)
+    for i in search(query,        # The query you want to run
+                    tld = 'com',  # The top level domain
+                    lang = 'en',  # The language
+                    num = 1,     # Number of results per page
+                    start = 0,    # First result to retrieve
+                    stop = 1,  # Last result to retrieve
+                    pause = 2.0,  # Lapse between HTTP requests
+                   ):
+        my_results_list.append(i)
+        print(i)    
 
-# Menards =====================================================
-my_results_list = []
+    # Menards =====================================================
+    my_results_list = []    
 
-query = "Menards WDT750SAHZ"
+    query = "Menards WDT750SAHZ"    
 
-for i in search(query,        # The query you want to run
-                tld = 'com',  # The top level domain
-                lang = 'en',  # The language
-                num = 1,     # Number of results per page
-                start = 0,    # First result to retrieve
-                stop = 1,  # Last result to retrieve
-                pause = 2.0,  # Lapse between HTTP requests
-               ):
-    my_results_list.append(i)
-    print(i)
+    for i in search(query,        # The query you want to run
+                    tld = 'com',  # The top level domain
+                    lang = 'en',  # The language
+                    num = 1,     # Number of results per page
+                    start = 0,    # First result to retrieve
+                    stop = 1,  # Last result to retrieve
+                    pause = 2.0,  # Lapse between HTTP requests
+                   ):
+        my_results_list.append(i)
+        print(i)    
+
+    get_price(my_results_list[0])
+
+def get_price(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+    if(url[len(url)-1] == "/"):
+        url = url[0:len(url)-1]
+        print(url)
+    req = requests.get(url, headers=headers)
+    pdb.set_trace()
+    soup = BeautifulSoup(req.content, 'html.parser')
+    print(soup.prettify())
+
+    return soup
+
+if __name__ == '__main__':
+    main()
